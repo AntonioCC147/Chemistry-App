272552acc650ad7e5953767dc1f10640880b4850
Crantea Antonio Cristian <thet0mmy@yahoo.com>
2023-04-09 19:19:27 +0300

    Set buttons

--- frontend/src/components/testeaza/functions.js
+++ frontend/src/components/testeaza/functions.js
@@ -1,5 +1,6 @@
 import LeftGlass from '../../assets/images/PaharVerde.png';
 import RightGlass from '../../assets/images/PaharAlbastru.png';
+import UndefinedGlass from '../../assets/images/UndefinedGlass.png';
 
 const SpawnLeftGlass = ({ name }) => {
     if(name !== "Undefined")
@@ -7,7 +8,9 @@ const SpawnLeftGlass = ({ name }) => {
             <img className="chem-glass" src={LeftGlass} alt="Glass"/>
     );
     else
-    return ( <p>Undefined</p> );
+        return (
+            <img className="chem-glass" src={UndefinedGlass} alt="Undefined Glass"/>
+    );
 }
 
 const SpawnRightGlass = ({ name }) => {
@@ -16,7 +19,25 @@ const SpawnRightGlass = ({ name }) => {
             <img className="chem-glass" src={RightGlass} alt="Glass"/>
     );
     else
-    return ( <p>Undefined</p> );
+        return (
+            <img className="chem-glass" src={UndefinedGlass} alt="Undefined Glass"/>
+    );
 }
 
-export { SpawnLeftGlass, SpawnRightGlass };
+const CheckInputs = ({ verify, glassLeft, elthPLeft, glassRight, elthPRight }) => {
+    if(verify === "Exist")
+        if(glassLeft !== "Undefined" && elthPLeft !== "Undefined" && glassRight !== "Undefined" && elthPRight !== "Undefined")
+            return (
+                <>
+                    Abc
+                </>
+            )
+}
+
+const SpawnSaltDeck = ({ name }) => {
+    if(name === "None")
+        return ( <>NONE</>);
+        else return ( <>EXIST</>);
+}
+
+export { SpawnLeftGlass, SpawnRightGlass, CheckInputs, SpawnSaltDeck };
--- frontend/src/components/testeaza/testeaza.js
+++ frontend/src/components/testeaza/testeaza.js
@@ -16,7 +16,7 @@ import NavbarComponent from '../navbar/navbar';
 import LeftGlass from '../../assets/images/PaharVerde.png';
 import RightGlass from '../../assets/images/PaharAlbastru.png';
 
-import { SpawnLeftGlass, SpawnRightGlass } from './functions.js';
+import { SpawnLeftGlass, SpawnRightGlass, CheckInputs, SpawnSaltDeck } from './functions.js';
 import './testeaza.css';
 
 export default function Testeaza() {
@@ -26,9 +26,12 @@ export default function Testeaza() {
     const [leftElthPName, setLeftElthPName] = useState("");
     const [rightElthPName, setRightElthPName] = useState("");
 
+    const [saltDeck, setSaltDeck] = useState("");
+
     useEffect(()=>{
 		setLeftGlassName("Undefined"); setRightGlassName("Undefined");
         setLeftElthPName("Undefined"); setRightElthPName("Undefined");
+        setSaltDeck("None");
 	}, [])
 
     return (
@@ -41,7 +44,7 @@ export default function Testeaza() {
                             <div className="mb-2 text-center pahare-dropdown">
                                 <ButtonGroup>
                                     <DropdownButton variant="success" id="dropdown-basic-button" title="Pahare" drop="end">
-                                        <Dropdown.Item eventKey="1" onClick={() => setLeftGlassName(<Latex>$Al(NO_3)_3$</Latex>)}><Latex>$Al(NO_3)_3$</Latex></Dropdown.Item>
+                                        <Dropdown.Item onClick={() => setLeftGlassName(<Latex>$Al(NO_3)_3$</Latex>)}><Latex>$Al(NO_3)_3$</Latex></Dropdown.Item>
                                         <Dropdown.Item onClick={() => setLeftGlassName(<Latex>$AgNO_3$</Latex>)}><Latex>$AgNO_3$</Latex></Dropdown.Item>
                                         <Dropdown.Item onClick={() => setLeftGlassName(<Latex>$Mg(NO_3)_2$</Latex>)}><Latex>$Mg(NO_3)_2$</Latex></Dropdown.Item>
                                         <Dropdown.Item onClick={() => setLeftGlassName(<Latex>$Cu(NO_3)_2$</Latex>)}><Latex>$Cu(NO_3)_2$</Latex></Dropdown.Item>
@@ -52,7 +55,7 @@ export default function Testeaza() {
                             <div className="mb-2 text-center placute-dropdown">
                                 <ButtonGroup>
                                     <DropdownButton variant="primary" id="dropdown-basic-button" title="Plăcuțe" drop="end">
-                                        <Dropdown.Item eventKey="1" onClick={() => setLeftElthPName(<Latex>$Al\;(Aluminiu)$</Latex>)}><Latex>$Al\;(Aluminiu)$</Latex></Dropdown.Item>
+                                        <Dropdown.Item onClick={() => setLeftElthPName(<Latex>$Al\;(Aluminiu)$</Latex>)}><Latex>$Al\;(Aluminiu)$</Latex></Dropdown.Item>
                                         <Dropdown.Item onClick={() => setLeftElthPName(<Latex>$Ag\;(Argint)$</Latex>)}><Latex>$Ag\;(Argint)$</Latex></Dropdown.Item>
                                         <Dropdown.Item onClick={() => setLeftElthPName(<Latex>$Mg\;(Magneziu)$</Latex>)}><Latex>$Mg\;(Magneziu)$</Latex></Dropdown.Item>
                                         <Dropdown.Item onClick={() => setLeftElthPName(<Latex>$Cu\;(Cupru)$</Latex>)}><Latex>$Cu\;(Cupru)$</Latex></Dropdown.Item>
@@ -61,13 +64,13 @@ export default function Testeaza() {
                                 </ButtonGroup>
                             </div>
                             <div className="mb-2 text-center">
-                                <Button variant="secondary">Punte de sare</Button>
+                                <Button variant="secondary" onClick={() => setSaltDeck("Exist")}>Punte de sare</Button>
                             </div> 
 
                             <img className="chem-glass" src={LeftGlass} alt="Glass"/>
 
                             <div className="text-center">
-                                <Button variant="danger" onClick={() => setLeftGlassName("Undefined")}>Resetează</Button>
+                                <Button variant="danger" onClick={() => { setLeftGlassName("Undefined"); setLeftElthPName("Undefined"); setSaltDeck("None") }}>Restează</Button>
                             </div>
                         </div>
                     </Col>
@@ -75,6 +78,15 @@ export default function Testeaza() {
 
                     <Col sm={8}>
                         <div className="work-space-box">
+                            <Row>
+                                <SpawnSaltDeck name={saltDeck}/>
+                                <CheckInputs
+                                    verify={saltDeck}
+                                    glassLeft={leftGlassName} elthPLeft={leftElthPName}
+                                    glassRight={rightGlassName} elthPRight={rightElthPName}
+                                />
+                            </Row>
+
                             <Row>
                                 <Col sm={6}>
                                     <div className="glass-box">
@@ -91,24 +103,16 @@ export default function Testeaza() {
 
                             <Row>
                                 <Col sm={3}>
-                                    <div className="pahar-box">
-                                        {leftGlassName}
-                                    </div>
+                                    <div className="pahar-box">{leftGlassName}</div>
                                 </Col>
                                 <Col sm={3}>
-                                    <div className="pile-box">
-                                        {leftElthPName}
-                                    </div>
+                                    <div className="pile-box">{leftElthPName}</div>
                                 </Col>
                                 <Col sm={3}>
-                                    <div className="pahar-box">
-                                        {rightGlassName}
-                                    </div>
+                                    <div className="pahar-box">{rightGlassName}</div>
                                 </Col>
                                 <Col sm={3}>
-                                    <div className="pile-box">
-                                        {rightElthPName}
-                                    </div>
+                                    <div className="pile-box">{rightElthPName}</div>
                                 </Col>
                             </Row>
 
@@ -124,7 +128,7 @@ export default function Testeaza() {
                             <div className="mb-2 text-center pahare-dropdown">
                                 <ButtonGroup>
                                 <DropdownButton variant="success" id="dropdown-basic-button" title="Pahare" drop="start">
-                                        <Dropdown.Item eventKey="1" onClick={() => setRightGlassName(<Latex>$Al(NO_3)_3$</Latex>)}><Latex>$Al(NO_3)_3$</Latex></Dropdown.Item>
+                                        <Dropdown.Item onClick={() => setRightGlassName(<Latex>$Al(NO_3)_3$</Latex>)}><Latex>$Al(NO_3)_3$</Latex></Dropdown.Item>
                                         <Dropdown.Item onClick={() => setRightGlassName(<Latex>$AgNO_3$</Latex>)}><Latex>$AgNO_3$</Latex></Dropdown.Item>
                                         <Dropdown.Item onClick={() => setRightGlassName(<Latex>$Mg(NO_3)_2$</Latex>)}><Latex>$Mg(NO_3)_2$</Latex></Dropdown.Item>
                                         <Dropdown.Item onClick={() => setRightGlassName(<Latex>$Cu(NO_3)_2$</Latex>)}><Latex>$Cu(NO_3)_2$</Latex></Dropdown.Item>
@@ -135,7 +139,7 @@ export default function Testeaza() {
                             <div className="mb-2 text-center placute-dropdown">
                                 <ButtonGroup>
                                     <DropdownButton variant="primary" id="dropdown-basic-button" title="Plăcuțe" drop="start">
-                                    <Dropdown.Item eventKey="1" onClick={() => setRightElthPName(<Latex>$Al\;(Aluminiu)$</Latex>)}><Latex>$Al\;(Aluminiu)$</Latex></Dropdown.Item>
+                                        <Dropdown.Item onClick={() => setRightElthPName(<Latex>$Al\;(Aluminiu)$</Latex>)}><Latex>$Al\;(Aluminiu)$</Latex></Dropdown.Item>
                                         <Dropdown.Item onClick={() => setRightElthPName(<Latex>$Ag\;(Argint)$</Latex>)}><Latex>$Ag\;(Argint)$</Latex></Dropdown.Item>
                                         <Dropdown.Item onClick={() => setRightElthPName(<Latex>$Mg\;(Magneziu)$</Latex>)}><Latex>$Mg\;(Magneziu)$</Latex></Dropdown.Item>
                                         <Dropdown.Item onClick={() => setRightElthPName(<Latex>$Cu\;(Cupru)$</Latex>)}><Latex>$Cu\;(Cupru)$</Latex></Dropdown.Item>
@@ -144,13 +148,13 @@ export default function Testeaza() {
                                 </ButtonGroup>
                             </div>
                             <div className="mb-2 text-center">
-                                <Button variant="secondary">Sterge puntea</Button>
+                                <Button variant="secondary" onClick={() => setSaltDeck("None")}>Sterge puntea</Button>
                             </div> 
 
                             <img className="chem-glass" src={RightGlass} alt="Glass"/>
 
                             <div className="text-center">
-                                <Button variant="danger" onClick={() => setRightGlassName("Undefined")}>Resetează</Button>
+                            <Button variant="danger" onClick={() => { setRightGlassName("Undefined"); setRightElthPName("Undefined"); }}>Resetează</Button>
                             </div>
                         </div>
                     </Col>
